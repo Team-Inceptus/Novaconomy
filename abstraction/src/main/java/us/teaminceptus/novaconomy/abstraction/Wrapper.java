c934963afa29981ba2792e978f62cff75f8a2113
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-06-13 21:35:34 -0500

    Update Wrapper.java
    - Refractor NBT Methods
    - CancelHolder Util Class

--- abstraction/src/main/java/us/teaminceptus/novaconomy/abstraction/Wrapper.java
+++ abstraction/src/main/java/us/teaminceptus/novaconomy/abstraction/Wrapper.java
@@ -6,6 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.serialization.ConfigurationSerializable;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryHolder;
@@ -17,14 +18,15 @@ import us.teaminceptus.novaconomy.api.Language;
 import us.teaminceptus.novaconomy.api.NovaConfig;
 import us.teaminceptus.novaconomy.api.business.Business;
 import us.teaminceptus.novaconomy.api.economy.Economy;
+import us.teaminceptus.novaconomy.api.util.BusinessProduct;
 import us.teaminceptus.novaconomy.api.util.Price;
-import us.teaminceptus.novaconomy.api.util.Product;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public interface Wrapper {
+
+    String ROOT = "Novaconomy";
 
     default int getCommandVersion() {
         return 1;
@@ -36,13 +38,25 @@ public interface Wrapper {
 
     String getNBTString(ItemStack item, String key);
 
-    void setNBTString(ItemStack item, String key, String value);
+    void setNBT(ItemStack item, String key, String value);
+
+    void setNBT(ItemStack item, String key, ItemStack value);
+
+    ItemStack getNBTItem(ItemStack item, String key);
 
     void openBook(Player p, ItemStack item);
 
     ItemStack getGUIBackground();
 
     ItemStack createSkull(OfflinePlayer p);
+
+    <T extends ConfigurationSerializable> T getNBTSerializable(ItemStack item, String key, Class<T> clazz);
+
+    void setNBT(ItemStack item, String key, ConfigurationSerializable serializable);
+
+    double getNBTDouble(ItemStack item, String key);
+
+    void setNBT(ItemStack item, String key, double value);
 
     static Plugin getPlugin() {
         return Bukkit.getPluginManager().getPlugin("Novaconomy");
@@ -59,8 +73,8 @@ public interface Wrapper {
         meta.setDisplayName("" + ChatColor.YELLOW + amount + econ.getSymbol());
         meta.setLore(Collections.singletonList(ChatColor.GOLD + "" + amount + " " + econ.getName() + "(s)"));
 
-        setNBTString(item, "economy", econ.getUniqueId().toString());
-        setNBTString(item, "amount", amount + "");
+        setNBT(item, "economy", econ.getUniqueId().toString());
+        setNBT(item, "amount", amount + "");
 
         item.setItemMeta(meta);
         return item;
@@ -80,37 +94,40 @@ public interface Wrapper {
         iMeta.setDisplayName(ChatColor.GOLD + b.getName());
         iMeta.setLore(Collections.singletonList(ChatColor.YELLOW + "" + b.getUniqueId()));
         icon.setItemMeta(iMeta);
+        inv.setItem(14, icon);
 
-        AtomicInteger slot = new AtomicInteger(19);
+        AtomicInteger slot = new AtomicInteger(28);
+        List<BusinessProduct> bProducts = b.getProducts();
         Map<ItemStack, List<Price>> products = new HashMap<>();
-        List<Product> bProducts = b.getProducts();
 
-        b.getProducts().forEach(p -> {
-            ItemStack item = p.getItem();
+        bProducts.forEach(p -> {
+            if (slot.get() == 35) slot.set(37);
+            if (slot.get() == 44) slot.set(46);
+            if (slot.get() >= 53) return;
+
+            ItemStack item = p.getItem().clone();
             Price price = p.getPrice();
+
             if (products.containsKey(item)) {
-                List<Price> prices = products.get(item);
+                List<Price> prices = new ArrayList<>(products.get(item));
                 prices.add(price);
                 products.put(item, prices);
             } else products.put(item, new ArrayList<Price>() {{ add(price); }});
-        });
 
-        bProducts.forEach(p -> {
-            if (slot.get() == 26) slot.set(28);
-            if (slot.get() == 35) slot.set(37);
-            if (slot.get() >= 44) return;
+            ItemStack product = item.clone();
 
-            ItemStack product = p.getItem().clone();
+            setNBT(product, "product", p);
+            setNBT(product, "id", "product:buy");
+
             ItemMeta meta = product.getItemMeta();
 
             List<String> lore = meta.hasLore() ? meta.getLore() : new ArrayList<>();
             lore.add(" ");
 
             AtomicInteger i = new AtomicInteger(0);
-            products.get(p.getItem()).forEach(pr -> {
-                lore.add(String.format(getMessage("constants.business.price"), i.get(), pr.getAmount(), p.getEconomy().getSymbol() ));
-                i.incrementAndGet();
-            });
+            products.get(item).forEach(pr ->
+                lore.add(String.format(get("constants.business.price"), i.incrementAndGet(), pr.getAmount(), p.getEconomy().getSymbol() ))
+            );
 
             meta.setLore(lore);
             product.setItemMeta(meta);
@@ -131,7 +148,6 @@ public interface Wrapper {
         if (size % 9 > 0) return null;
 
         Inventory inv = Bukkit.createInventory(holder, size, name);
-
         ItemStack guiBG = getGUIBackground();
 
         if (size < 27) return inv;
@@ -183,5 +199,13 @@ public interface Wrapper {
     }
 
     static String getMessage(String key) { return get("plugin.prefix") + get(key); }
+
+    class CancelHolder implements InventoryHolder {
+
+        @Override
+        public Inventory getInventory() {
+            return null;
+        }
+    }
 
 }
