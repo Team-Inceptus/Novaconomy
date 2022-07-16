dfb97cd7029f6616546c65f551b4004d9e959e95
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-07-16 12:31:55 -0500

    Update VaultEconomy.java
    Move Methods

--- plugin/src/main/java/us/teaminceptus/novaconomy/vault/VaultEconomy.java
+++ plugin/src/main/java/us/teaminceptus/novaconomy/vault/VaultEconomy.java
@@ -1,21 +1,14 @@
 package us.teaminceptus.novaconomy.vault;
 
-import com.google.gson.Gson;
 import net.milkbowl.vault.economy.AbstractEconomy;
 import net.milkbowl.vault.economy.EconomyResponse;
-import org.bukkit.Bukkit;
-import org.bukkit.OfflinePlayer;
 import us.teaminceptus.novaconomy.api.NovaPlayer;
 import us.teaminceptus.novaconomy.api.economy.Economy;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.List;
-import java.util.UUID;
+
+import static us.teaminceptus.novaconomy.Novaconomy.getPlayer;
 
 class VaultEconomy extends AbstractEconomy implements net.milkbowl.vault.economy.Economy {
 
@@ -81,58 +74,6 @@ class VaultEconomy extends AbstractEconomy implements net.milkbowl.vault.economy
     @Override
     public boolean hasAccount(String playerName, String worldName) {
         return true;
-    }
-
-    private static class APIPlayer {
-
-        public final String name;
-        public final String id;
-
-        public APIPlayer(String name, String id) {
-            this.name = name;
-            this.id = id;
-        }
-
-    }
-
-    private static OfflinePlayer getPlayer(String name) {
-        if (Bukkit.getOnlineMode()) try {
-            URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("User-Agent", "Java 8 PlutoChat Bot");
-
-            int responseCode = connection.getResponseCode();
-
-            if (responseCode == HttpURLConnection.HTTP_OK || responseCode == HttpURLConnection.HTTP_ACCEPTED) {
-                BufferedReader input = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder builder = new StringBuilder();
-                String inputLine;
-                while ((inputLine = input.readLine()) != null) builder.append(inputLine);
-
-                Gson g = new Gson();
-                return Bukkit.getOfflinePlayer(untrimUUID(g.fromJson(builder.toString(), APIPlayer.class).id));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        else
-            return Bukkit.getPlayer(UUID.nameUUIDFromBytes(("OfflinePlayer:" + name).getBytes(StandardCharsets.UTF_8)));
-        return null;
-    }
-
-    public static UUID untrimUUID(String oldUUID) {
-        String p1 = oldUUID.substring(0, 8);
-        String p2 = oldUUID.substring(8, 12);
-        String p3 = oldUUID.substring(12, 16);
-        String p4 = oldUUID.substring(16, 20);
-        String p5 = oldUUID.substring(20, 32);
-
-        String newUUID = p1 + "-" + p2 + "-" + p3 + "-" + p4 + "-" + p5;
-
-        return UUID.fromString(newUUID);
     }
 
     @Override
