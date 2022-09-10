44ecfb84fad143dbc88ae46b1bc19d15b69ccde7
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2022-09-10 00:16:48 -0500

    Update BusinessStatistics.java
    - Add Business Field to Transactions
    - Remove Externalizable from Transaction and Serializable from BusinessStatistics

--- api/src/main/java/us/teaminceptus/novaconomy/api/business/BusinessStatistics.java
+++ api/src/main/java/us/teaminceptus/novaconomy/api/business/BusinessStatistics.java
@@ -2,21 +2,16 @@ package us.teaminceptus.novaconomy.api.business;
 
 import com.google.common.base.Preconditions;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.serialization.ConfigurationSerializable;
-import org.bukkit.configuration.serialization.DelegateDeserialization;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import us.teaminceptus.novaconomy.api.NovaConfig;
 import us.teaminceptus.novaconomy.api.economy.Economy;
+import us.teaminceptus.novaconomy.api.util.BusinessProduct;
 import us.teaminceptus.novaconomy.api.util.Price;
 import us.teaminceptus.novaconomy.api.util.Product;
 
-import java.io.*;
-import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -25,15 +20,16 @@ import java.util.UUID;
 /**
  * Utility Class for Business Statistics
  */
-public final class BusinessStatistics implements ConfigurationSerializable, Serializable {
+public final class BusinessStatistics implements ConfigurationSerializable {
 
     /**
      * Represents a Transaction for a Business
      */
-    public static final class Transaction implements ConfigurationSerializable, Externalizable {
+    public static final class Transaction implements ConfigurationSerializable {
         private UUID buyer;
         private Product product;
         private long timestamp;
+        private UUID business;
 
         /**
          * Constructs a Transaction with no buyer or product.
@@ -46,34 +42,58 @@ public final class BusinessStatistics implements ConfigurationSerializable, Seri
          * Constructs a Transaction with a timestamp equaling {@link System#currentTimeMillis()}.
          * @param buyer Buyer of the Product
          * @param product Product bought
+         * @param business Business that the transaction took place in
          */
-        public Transaction(@Nullable OfflinePlayer buyer, @Nullable Product product) {
-            this(buyer, product, System.currentTimeMillis());
+        public Transaction(@Nullable OfflinePlayer buyer, @Nullable Business business, @Nullable Product product) {
+            this(buyer, business, product, System.currentTimeMillis());
+        }
+
+        /**
+         * Constructs a Transaction with a timestamp equaling {@link System#currentTimeMillis()}.
+         * @param buyer Buyer of the Product
+         * @param product Business Product bought
+         */
+        public Transaction(@Nullable OfflinePlayer buyer, @Nullable BusinessProduct product) {
+            this(buyer, product.getBusiness(), new Product(product), System.currentTimeMillis());
         }
 
         /**
          * Constructs a Transaction.
          * @param buyer Player who bought the Product
+         * @param business Business that the transaction took place in
          * @param product Product that was bought
          * @param timestamp Time that the Transaction was made
          * @throws IllegalArgumentException if timestamp is negative
          */
-        public Transaction(@Nullable OfflinePlayer buyer, @Nullable Product product, long timestamp) throws IllegalArgumentException {
+        public Transaction(@Nullable OfflinePlayer buyer, @Nullable Business business, @Nullable Product product, long timestamp) throws IllegalArgumentException {
             if (timestamp < 0) throw new IllegalArgumentException("Timestamp cannot be negative");
             this.buyer = buyer == null ? null : buyer.getUniqueId();
             this.product = product;
             this.timestamp = timestamp;
+            this.business = business.getUniqueId();
         }
 
         /**
          * Constructs a Transaction.
          * @param buyer Player who bought the Product
+         * @param product Business Product that was bought
+         * @param timestamp Time that the Transaction was made
+         * @throws IllegalArgumentException if timestamp is negative
+         */
+        public Transaction(@Nullable OfflinePlayer buyer, @Nullable BusinessProduct product, long timestamp) throws IllegalArgumentException {
+            this(buyer, product.getBusiness(), new Product(product), timestamp);
+        }
+
+        /**
+         * Constructs a Transaction.
+         * @param buyer Player who bought the Product
+         * @param business Business that the transaction took place in
          * @param product Product that was bought
          * @param timestamp Date Object of the time that the Transaction was made
          * @throws NullPointerException if Date is null
          */
-        public Transaction(@Nullable OfflinePlayer buyer, @Nullable Product product, @NotNull Date timestamp) throws NullPointerException {
-            this(buyer, product, timestamp.getTime());
+        public Transaction(@Nullable OfflinePlayer buyer, @Nullable Business business, @Nullable Product product, @NotNull Date timestamp) throws NullPointerException {
+            this(buyer, business, product, timestamp.getTime());
         }
 
         /**
@@ -139,6 +159,23 @@ public final class BusinessStatistics implements ConfigurationSerializable, Seri
             setTimestamp(timestamp.getTime());
         }
 
+        /**
+         * Returns the Business that the transaction took place in.
+         * @return Business that the transaction took place in.
+         */
+        @Nullable
+        public Business getBusiness() {
+            return Business.getById(business);
+        }
+
+        /**
+         * Sets the business of this transaction.
+         * @param business Business to set
+         */
+        public void setBusiness(@NotNull Business business) {
+            this.business = business.getUniqueId();
+        }
+
         @Override
         public Map<String, Object> serialize() {
             return new HashMap<String, Object>() {{
@@ -147,6 +184,7 @@ public final class BusinessStatistics implements ConfigurationSerializable, Seri
                 put("economy", product == null ? null : product.getEconomy().getUniqueId().toString());
                 put("amount", product == null ? null : product.getAmount());
                 put("timestamp", timestamp);
+                put("business", business == null ? null : business.toString());
             }};
         }
 
@@ -165,9 +203,11 @@ public final class BusinessStatistics implements ConfigurationSerializable, Seri
             Object buyerO = serial.get("buyer");
             OfflinePlayer buyer = buyerO instanceof OfflinePlayer ? (OfflinePlayer) buyerO : Bukkit.getOfflinePlayer(UUID.fromString((String) buyerO));
 
+            Object businessO = serial.get("business");
+            Business business = businessO == null ? null : Business.getById(UUID.fromString(businessO.toString()));
+
             try {
-                return new Transaction(
-                        buyer,
+                return new Transaction(buyer, business,
                         new Product(
                                 (ItemStack) serial.get("item"),
                                 new Price(
@@ -179,47 +219,6 @@ public final class BusinessStatistics implements ConfigurationSerializable, Seri
             } catch (NullPointerException | ClassCastException e) {
                 throw new IllegalArgumentException(e);
             }
-        }
-
-        // Serialization
-
-        @Override
-        public void writeExternal(ObjectOutput out) throws IOException {
-            out.writeObject(buyer);
-
-            Map<String, Object> item = new HashMap<>(product.getItem().serialize());
-            item.put("meta", product.getItem().getItemMeta().serialize());
-
-            out.writeObject(item);
-            out.writeObject(product.getPrice());
-            out.writeLong(timestamp);
-        }
-
-        @Override
-        @SuppressWarnings("unchecked")
-        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
-            this.buyer = (UUID) in.readObject();
-
-            Map<String, Object> item = new HashMap<>((Map<String, Object>) in.readObject());
-
-            ItemStack prItem = null;
-
-            try {
-                ItemMeta base = Bukkit.getItemFactory().getItemMeta(Material.valueOf((String) item.get("type")));
-                DelegateDeserialization deserialization = base.getClass().getAnnotation(DelegateDeserialization.class);
-                Method deserialize = deserialization.value().getDeclaredMethod("deserialize", Map.class);
-                deserialize.setAccessible(true);
-
-                ItemMeta meta = (ItemMeta) deserialize.invoke(null, item.get("meta"));
-                item.put("meta", meta);
-                prItem = ItemStack.deserialize(item);
-            } catch (ReflectiveOperationException e) {
-                NovaConfig.print(e);
-            }
-
-            this.product = new Product(prItem, (Price) in.readObject());
-
-            this.timestamp = in.readLong();
         }
     }
 
@@ -241,61 +240,6 @@ public final class BusinessStatistics implements ConfigurationSerializable, Seri
 
     BusinessStatistics(Business business) {
         this.businessId = business.getUniqueId();
-    }
-
-    void writeStats(ObjectOutputStream os) throws IOException {
-        os.writeObject(businessId);
-        os.writeObject(lastTransaction);
-        os.writeInt(totalSales);
-        os.writeInt(totalResources);
-
-        Map<Map<String, Object>, Integer> prods = new HashMap<>();
-        for (Map.Entry<Product, Integer> entry : productSales.entrySet()) {
-            Product p = entry.getKey();
-            Map<String, Object> m = new HashMap<>(p.serialize());
-
-            Map<String, Object> item = new HashMap<>(p.getItem().serialize());
-            item.put("meta", p.getItem().getItemMeta().serialize());
-            m.put("item", item);
-
-            prods.put(m, entry.getValue());
-        }
-        os.writeObject(prods);
-        os.writeInt(views);
-    }
-
-    @SuppressWarnings("unchecked")
-    static BusinessStatistics readStats(ObjectInputStream is) throws IOException, ClassNotFoundException {
-        BusinessStatistics stats = new BusinessStatistics((UUID) is.readObject());
-        stats.lastTransaction = (Transaction) is.readObject();
-        stats.totalSales = is.readInt();
-        stats.totalResources = is.readInt();
-
-        try {
-            Map<Map<String, Object>, Integer> prods = (Map<Map<String, Object>, Integer>) is.readObject();
-            for (Map.Entry<Map<String, Object>, Integer> entry : prods.entrySet()) {
-                Map<String, Object> m = entry.getKey();
-
-                Map<String, Object> item = new HashMap<>((Map<String, Object>) m.get("item"));
-
-                ItemMeta base = Bukkit.getItemFactory().getItemMeta(Material.valueOf((String) item.get("type")));
-                DelegateDeserialization deserialization = base.getClass().getAnnotation(DelegateDeserialization.class);
-                Method deserialize = deserialization.value().getDeclaredMethod("deserialize", Map.class);
-
-                deserialize.setAccessible(true);
-                item.put("meta", deserialize.invoke(null, item.get("meta")));
-
-                ItemStack prItem = ItemStack.deserialize(item);
-
-                stats.productSales.put(new Product(prItem, (Price) m.get("price")), entry.getValue());
-            }
-        } catch (ReflectiveOperationException e) {
-            NovaConfig.print(e);
-        }
-
-        stats.views = is.available() > 0 ? is.readInt() : 0;
-
-        return stats;
     }
 
     /**
@@ -397,7 +341,6 @@ public final class BusinessStatistics implements ConfigurationSerializable, Seri
     }
 
     @Override
-    @Deprecated
     public Map<String, Object> serialize() {
         return new HashMap<String, Object>() {{
             put("business_id", businessId.toString());
@@ -414,12 +357,10 @@ public final class BusinessStatistics implements ConfigurationSerializable, Seri
     /**
      * Deserializes a Map into a BusinessStatistics.
      * @param serial Serialization from {@link #serialize()}
-     * @deprecated Bukkit Serialization is no longer used
      * @return Deserialized BusinessStatistics, or null if serial is null
      * @throws IllegalArgumentException if an argument is malformed
      * @throws NullPointerException if ID is missing
      */
-    @Deprecated
     @Nullable
     @SuppressWarnings("unchecked")
     public static BusinessStatistics deserialize(@NotNull Map<String, Object> serial) throws IllegalArgumentException, NullPointerException {
