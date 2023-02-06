3e07c6cdc6bfb2b19febbefe4f2a80310bbb39b3
GamerCoder215 <54124162+GamerCoder215@users.noreply.github.com>
2023-02-05 21:07:36 -0600

    Update SortingType.java
    Create Product Sorting Types, SortingType#values

--- api/src/main/java/us/teaminceptus/novaconomy/api/SortingType.java
+++ api/src/main/java/us/teaminceptus/novaconomy/api/SortingType.java
@@ -1,12 +1,13 @@
 package us.teaminceptus.novaconomy.api;
 
 import org.jetbrains.annotations.NotNull;
-
 import us.teaminceptus.novaconomy.api.business.Business;
 import us.teaminceptus.novaconomy.api.economy.Economy;
+import us.teaminceptus.novaconomy.api.util.BusinessProduct;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -17,7 +18,7 @@ import java.util.List;
  */
 @FunctionalInterface
 public interface SortingType<T> extends Comparator<T> {
-   
+
     // Economies
 
     /**
@@ -34,7 +35,6 @@ public interface SortingType<T> extends Comparator<T> {
      * Sorts an economy by whether it has natural causes.
      */
     SortingType<Economy> ECONOMY_NATURAL_CAUSES = (e1, e2) -> Boolean.compare(e1.hasNaturalIncrease(), e2.hasNaturalIncrease());
-
 
     // Business
 
@@ -58,11 +58,52 @@ public interface SortingType<T> extends Comparator<T> {
      */
     SortingType<Business> BUSINESS_CREATION_DATE_DESCENDING = BUSINESS_CREATION_DATE_ASCENDING.reversed();
 
+    // Business Products
 
+    /**
+     * Sorts a product by its name in ascending order.
+     */
+    SortingType<BusinessProduct> PRODUCT_NAME_ASCENDING = (p1, p2) -> p1.getItem().getType().name().compareTo(p1.getItem().getType().name());
+
+    /**
+     * Sorts a product by its name in descending order.
+     */
+    SortingType<BusinessProduct> PRODUCT_NAME_DESCENDING = PRODUCT_NAME_ASCENDING.reversed();
+
+    /**
+     * Sorts a product by its price in ascending order.
+     */
+    SortingType<BusinessProduct> PRODUCT_PRICE_ASCENDING = (p1, p2) -> p2.getPrice().compareTo(p1.getPrice());
+
+    /**
+     * Sorts a product by its price in descending order.
+     */
+    SortingType<BusinessProduct> PRODUCT_PRICE_DESCENDING = PRODUCT_PRICE_ASCENDING.reversed();
+
+    /**
+     * Sorts a business product by its stock in ascending order.
+     */
+    SortingType<BusinessProduct> PRODUCT_STOCK_ASCENDING = (p1, p2) -> {
+        Business b = p1.getBusiness();
+        return Integer.compare(b.getTotalStock(p1.getItem()), b.getTotalStock(p2.getItem()));
+    };
+
+    /**
+     * Sorts a business product by its stock in descending order.
+     */
+    SortingType<BusinessProduct> PRODUCT_STOCK_DESCENDING = PRODUCT_STOCK_ASCENDING.reversed();
 
     @Override
     default SortingType<T> reversed() {
         return (o1, o2) -> compare(o2, o1);
+    }
+
+    @Override
+    default SortingType<T> thenComparing(Comparator<? super T> other) {
+        return (o1, o2) -> {
+            int result = compare(o1, o2);
+            return result != 0 ? result : other.compare(o1, o2);
+        };
     }
 
     /**
@@ -81,6 +122,38 @@ public interface SortingType<T> extends Comparator<T> {
                 if (!SortingType.class.isAssignableFrom(f.getType())) continue;
 
                 values.add((SortingType<?>) f.get(null));
+            }
+
+            return values.toArray(new SortingType[0]);
+        } catch (ReflectiveOperationException e) {
+            NovaConfig.print(e);
+        }
+
+        throw new RuntimeException("Failed to fetch SortingType values");
+    }
+
+    /**
+     * Fetches all SortingType values of a specific type.
+     * @param clazz Type of SortingType
+     * @return SortingType values
+     * @param <T> Type of SortingType
+     */
+    @NotNull
+    @SuppressWarnings("unchecked")
+    static <T> SortingType<T>[] values(@NotNull Class<T> clazz) {
+        try {
+            List<SortingType<T>> values = new ArrayList<>();
+
+            for (Field f : SortingType.class.getDeclaredFields()) {
+                if (!Modifier.isStatic(f.getModifiers())) continue;
+                if (!Modifier.isFinal(f.getModifiers())) continue;
+                if (!Modifier.isPublic(f.getModifiers())) continue;
+                if (!SortingType.class.isAssignableFrom(f.getType())) continue;
+
+                ParameterizedType type = (ParameterizedType) f.getGenericType();
+                Class<?> fClass = (Class<?>) type.getActualTypeArguments()[0];
+                if (clazz.isAssignableFrom(fClass))
+                    values.add((SortingType<T>) f.get(null));
             }
 
             return values.toArray(new SortingType[0]);
