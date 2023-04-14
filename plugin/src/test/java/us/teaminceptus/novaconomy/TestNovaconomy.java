9852b8ff6ef9ff40e282bbd1f5d7138bea474fd8
GamerCoder <gamercoder215@gmail.com>
2023-04-14 02:14:07 +0000

    Create TestNovaconomy.java

---
+++ plugin/src/test/java/us/teaminceptus/novaconomy/TestNovaconomy.java
@@ -0,0 +1,19 @@
+package us.teaminceptus.novaconomy;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import us.teaminceptus.novaconomy.api.economy.market.MarketCategory;
+
+public class TestNovaconomy {
+    
+    @Test
+    @DisplayName("Test Market#basePrices")
+    public void testMarketPrices() {
+        // TODO Create Base Prices
+        // for (MarketCategory c : MarketCategory.values())
+        //     Assertions.assertTrue(Novaconomy.basePrices.keySet().containsAll(c.getItemNames()));
+    }
+
+}
